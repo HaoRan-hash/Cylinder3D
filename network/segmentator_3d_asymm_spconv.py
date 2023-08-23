0276f4a4d0f5eb1ac48ff7dbfb48ec4f893e52b4
chenhaoran <2627963568@qq.com>
2023-08-23 23:49:49 +0800

    spconv 1 to 2

--- network/segmentator_3d_asymm_spconv.py
+++ network/segmentator_3d_asymm_spconv.py
@@ -7,6 +7,7 @@ import spconv
 import torch
 from torch import nn
 import spconv.pytorch as spconv
+from spconv.pytorch import functional as Fsp
 
 
 def conv3x3(in_planes, out_planes, stride=1, indice_key=None):
@@ -45,23 +46,21 @@ def conv1x1(in_planes, out_planes, stride=1, indice_key=None):
 
 
 class ResContextBlock(nn.Module):
-    def __init__(self, in_filters, out_filters, kernel_size=(3, 3, 3), stride=1, indice_key=None):
+    def __init__(self, in_filters, out_filters, indice_key=None):
         super(ResContextBlock, self).__init__()
-        self.conv1 = conv1x3(in_filters, out_filters, indice_key=indice_key + "bef")
-        self.bn0 = nn.BatchNorm1d(out_filters)
-        self.act1 = nn.LeakyReLU()
-
-        self.conv1_2 = conv3x1(out_filters, out_filters, indice_key=indice_key + "bef")
-        self.bn0_2 = nn.BatchNorm1d(out_filters)
-        self.act1_2 = nn.LeakyReLU()
-
-        self.conv2 = conv3x1(in_filters, out_filters, indice_key=indice_key + "bef")
-        self.act2 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm1d(out_filters)
-
-        self.conv3 = conv1x3(out_filters, out_filters, indice_key=indice_key + "bef")
-        self.act3 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm1d(out_filters)
+        
+        self.conv1 = spconv.SparseSequential(conv1x3(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU(),
+                                             conv3x1(out_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU())
+        self.conv2 = spconv.SparseSequential(conv3x1(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU(),
+                                             conv1x3(out_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU())
 
         self.weight_initialization()
 
@@ -73,21 +72,8 @@ class ResContextBlock(nn.Module):
 
     def forward(self, x):
         shortcut = self.conv1(x)
-        shortcut.features = self.act1(shortcut.features)
-        shortcut.features = self.bn0(shortcut.features)
-
-        shortcut = self.conv1_2(shortcut)
-        shortcut.features = self.act1_2(shortcut.features)
-        shortcut.features = self.bn0_2(shortcut.features)
-
         resA = self.conv2(x)
-        resA.features = self.act2(resA.features)
-        resA.features = self.bn1(resA.features)
-
-        resA = self.conv3(resA)
-        resA.features = self.act3(resA.features)
-        resA.features = self.bn2(resA.features)
-        resA.features = resA.features + shortcut.features
+        resA = Fsp.sparse_add(resA, shortcut)
 
         return resA
 
@@ -98,22 +84,20 @@ class ResBlock(nn.Module):
         super(ResBlock, self).__init__()
         self.pooling = pooling
         self.drop_out = drop_out
-
-        self.conv1 = conv3x1(in_filters, out_filters, indice_key=indice_key + "bef")
-        self.act1 = nn.LeakyReLU()
-        self.bn0 = nn.BatchNorm1d(out_filters)
-
-        self.conv1_2 = conv1x3(out_filters, out_filters, indice_key=indice_key + "bef")
-        self.act1_2 = nn.LeakyReLU()
-        self.bn0_2 = nn.BatchNorm1d(out_filters)
-
-        self.conv2 = conv1x3(in_filters, out_filters, indice_key=indice_key + "bef")
-        self.act2 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm1d(out_filters)
-
-        self.conv3 = conv3x1(out_filters, out_filters, indice_key=indice_key + "bef")
-        self.act3 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm1d(out_filters)
+        
+        self.conv1 = spconv.SparseSequential(conv3x1(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU(),
+                                             conv1x3(out_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU())
+        
+        self.conv2 = spconv.SparseSequential(conv1x3(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU(),
+                                             conv3x1(out_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU())
 
         if pooling:
             if height_pooling:
@@ -132,22 +116,8 @@ class ResBlock(nn.Module):
 
     def forward(self, x):
         shortcut = self.conv1(x)
-        shortcut.features = self.act1(shortcut.features)
-        shortcut.features = self.bn0(shortcut.features)
-
-        shortcut = self.conv1_2(shortcut)
-        shortcut.features = self.act1_2(shortcut.features)
-        shortcut.features = self.bn0_2(shortcut.features)
-
         resA = self.conv2(x)
-        resA.features = self.act2(resA.features)
-        resA.features = self.bn1(resA.features)
-
-        resA = self.conv3(resA)
-        resA.features = self.act3(resA.features)
-        resA.features = self.bn2(resA.features)
-
-        resA.features = resA.features + shortcut.features
+        resA = Fsp.sparse_add(resA, shortcut)
 
         if self.pooling:
             resB = self.pool(resA)
@@ -159,26 +129,24 @@ class ResBlock(nn.Module):
 class UpBlock(nn.Module):
     def __init__(self, in_filters, out_filters, kernel_size=(3, 3, 3), indice_key=None, up_key=None):
         super(UpBlock, self).__init__()
-        # self.drop_out = drop_out
-        self.trans_dilao = conv3x3(in_filters, out_filters, indice_key=indice_key + "new_up")
-        self.trans_act = nn.LeakyReLU()
-        self.trans_bn = nn.BatchNorm1d(out_filters)
-
-        self.conv1 = conv1x3(out_filters, out_filters, indice_key=indice_key)
-        self.act1 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm1d(out_filters)
-
-        self.conv2 = conv3x1(out_filters, out_filters, indice_key=indice_key)
-        self.act2 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm1d(out_filters)
-
-        self.conv3 = conv3x3(out_filters, out_filters, indice_key=indice_key)
-        self.act3 = nn.LeakyReLU()
-        self.bn3 = nn.BatchNorm1d(out_filters)
-        # self.dropout3 = nn.Dropout3d(p=dropout_rate)
-
+        
+        self.trans = spconv.SparseSequential(conv3x3(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.LeakyReLU())
+        
         self.up_subm = spconv.SparseInverseConv3d(out_filters, out_filters, kernel_size=3, indice_key=up_key,
                                                   bias=False)
+        
+        self.conv = spconv.SparseSequential(conv1x3(out_filters, out_filters),
+                                            nn.BatchNorm1d(out_filters),
+                                            nn.LeakyReLU(),
+                                            conv3x1(out_filters, out_filters),
+                                            nn.BatchNorm1d(out_filters),
+                                            nn.LeakyReLU(),
+                                            conv3x3(out_filters, out_filters),
+                                            nn.BatchNorm1d(out_filters),
+                                            nn.LeakyReLU())
+        # self.dropout3 = nn.Dropout3d(p=dropout_rate)
 
         self.weight_initialization()
 
@@ -189,60 +157,39 @@ class UpBlock(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x, skip):
-        upA = self.trans_dilao(x)
-        upA.features = self.trans_act(upA.features)
-        upA.features = self.trans_bn(upA.features)
+        upA = self.trans(x)
 
-        ## upsample
+        # upsample
         upA = self.up_subm(upA)
+        upA = upA.replace_feature(upA.features + skip.features)
 
-        upA.features = upA.features + skip.features
-
-        upE = self.conv1(upA)
-        upE.features = self.act1(upE.features)
-        upE.features = self.bn1(upE.features)
-
-        upE = self.conv2(upE)
-        upE.features = self.act2(upE.features)
-        upE.features = self.bn2(upE.features)
-
-        upE = self.conv3(upE)
-        upE.features = self.act3(upE.features)
-        upE.features = self.bn3(upE.features)
-
+        upE = self.conv(upA)
         return upE
 
 
 class ReconBlock(nn.Module):
     def __init__(self, in_filters, out_filters, kernel_size=(3, 3, 3), stride=1, indice_key=None):
         super(ReconBlock, self).__init__()
-        self.conv1 = conv3x1x1(in_filters, out_filters, indice_key=indice_key + "bef")
-        self.bn0 = nn.BatchNorm1d(out_filters)
-        self.act1 = nn.Sigmoid()
 
-        self.conv1_2 = conv1x3x1(in_filters, out_filters, indice_key=indice_key + "bef")
-        self.bn0_2 = nn.BatchNorm1d(out_filters)
-        self.act1_2 = nn.Sigmoid()
-
-        self.conv1_3 = conv1x1x3(in_filters, out_filters, indice_key=indice_key + "bef")
-        self.bn0_3 = nn.BatchNorm1d(out_filters)
-        self.act1_3 = nn.Sigmoid()
+        self.conv1 = spconv.SparseSequential(conv3x1x1(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.Sigmoid())
+        
+        self.conv2 = spconv.SparseSequential(conv1x3x1(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.Sigmoid())
+        
+        self.conv3 = spconv.SparseSequential(conv1x1x3(in_filters, out_filters),
+                                             nn.BatchNorm1d(out_filters),
+                                             nn.Sigmoid())
 
     def forward(self, x):
         shortcut = self.conv1(x)
-        shortcut.features = self.bn0(shortcut.features)
-        shortcut.features = self.act1(shortcut.features)
-
-        shortcut2 = self.conv1_2(x)
-        shortcut2.features = self.bn0_2(shortcut2.features)
-        shortcut2.features = self.act1_2(shortcut2.features)
-
-        shortcut3 = self.conv1_3(x)
-        shortcut3.features = self.bn0_3(shortcut3.features)
-        shortcut3.features = self.act1_3(shortcut3.features)
-        shortcut.features = shortcut.features + shortcut2.features + shortcut3.features
-
-        shortcut.features = shortcut.features * x.features
+        shortcut2 = self.conv2(x)
+        shortcut3 = self.conv3(x)
+        shortcut = Fsp.sparse_add(shortcut, shortcut2, shortcut3)
+        
+        shortcut = shortcut.replace_feature(shortcut.features * x.features)
 
         return shortcut
 
@@ -282,7 +229,6 @@ class Asymm_3d_spconv(nn.Module):
                                         bias=True)
 
     def forward(self, voxel_features, coors, batch_size):
-        # x = x.contiguous()
         coors = coors.int()
         # import pdb
         # pdb.set_trace()
@@ -301,7 +247,7 @@ class Asymm_3d_spconv(nn.Module):
 
         up0e = self.ReconNet(up1e)
 
-        up0e.features = torch.cat((up0e.features, up1e.features), 1)
+        up0e = up0e.replace_feature(torch.cat((up0e.features, up1e.features), dim=1))
 
         logits = self.logits(up0e)
         y = logits.dense()
